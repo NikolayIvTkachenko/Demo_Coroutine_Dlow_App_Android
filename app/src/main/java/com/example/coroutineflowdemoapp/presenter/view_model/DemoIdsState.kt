8f70b3da631f9d3c2e2a6d7542a33d114b2d32f0
NikolayTkachenko <tkachenni@mail.ru>
2025-06-05 21:30:48 +0300

    update commit

--- app/src/main/java/com/example/coroutineflowdemoapp/presenter/view_model/DemoIdsState.kt
+++ app/src/main/java/com/example/coroutineflowdemoapp/presenter/view_model/DemoIdsState.kt
@@ -1,7 +1,7 @@
 package com.example.coroutineflowdemoapp.presenter.view_model
 
 data class DemoIdsState(
-    val listIds: List<String>,
+    val listIds: List<String>? = null,
     val isLoading: Boolean = false,
     val error: String? = null,
 )
--- app/src/main/java/com/example/coroutineflowdemoapp/presenter/view_model/DemoListIdsViewModel.kt
+++ app/src/main/java/com/example/coroutineflowdemoapp/presenter/view_model/DemoListIdsViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.coroutineflowdemoapp.presenter.view_model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coroutineflowdemoapp.domain.repository.DemoTasksRawRepository
 import com.example.coroutineflowdemoapp.domain.repository.DemoTasksRepository
+import com.example.coroutineflowdemoapp.domain.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,19 +18,46 @@ class DemoListIdsViewModel @Inject constructor(
     private val repositoryRaw: DemoTasksRawRepository,
 ): ViewModel() {
 
+    var state by mutableStateOf(DemoIdsState())
+        private set
+
     //Метод, который вызывает список идентификаторов
     fun getIdsInResource() {
         println("DemoListIdsViewModel getIdsInResource()")
         viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+                error = null
+            )
             val startTime = System.currentTimeMillis()
             println()
-            val ids = repository.getListIds()
+            val result = repository.getListIds()
             val endTime = System.currentTimeMillis()
             val timeWork = endTime - startTime
             println("startTime = $startTime")
             println("endTime = $endTime")
             println("timeWork = $timeWork")
-            println("ids = $ids")
+            when(result) {
+                is Resource.Success -> {
+                    println("Resource.Success ->")
+                    val ids = result.data
+                    val message = result.message
+                    println("ids = $ids")
+                    println("message = $message")
+                    state = state.copy(
+                        listIds = ids,
+                        isLoading = false,
+                        error = null
+                    )
+                }
+                is Resource.Error -> {
+                    println("Resource.Error ->")
+                    state = state.copy(
+                        isLoading = false,
+                        error = result.message,
+                    )
+                }
+            }
         }
     }
 
